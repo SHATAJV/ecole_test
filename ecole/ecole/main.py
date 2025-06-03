40ce2a00d405b7a9aebdc6c370c8bc2d8f51c522
shahrzad Tajvidi <shahrzad.tajvidi@fms-ea.com>
2025-06-03 14:40:44 +0200

    add update main

--- ecole/ecole/main.py
+++ ecole/ecole/main.py
@@ -17,48 +17,58 @@ from business.school import School
 def init_school(school: School) -> None:
     """Initialisation d'un jeu de test pour l'école school."""
     # création des étudiants et rattachement à leur adresse
-    paul: Student    = Student('Paul', 'Dubois', 12)
-    valerie: Student = Student('Valérie', 'Dumont', 13)
-    louis: Student   = Student('Louis', 'Berthot', 11)
+    paul = Student('Paul', 'Dubois', 12)
+    valerie = Student('Valérie', 'Dumont', 13)
+    louis = Student('Louis', 'Berthot', 11)
 
-    paul.address    = Address('12 rue des Pinsons', 'Castanet', 31320)
+    paul.address = Address('12 rue des Pinsons', 'Castanet', 31320)
     valerie.address = Address('43 avenue Jean Zay', 'Toulouse', 31200)
-    louis.address   = Address('7 impasse des Coteaux', 'Cornebarrieu', 31150)
+    louis.address = Address('7 impasse des Coteaux', 'Cornebarrieu', 31150)
 
     # ajout de ceux-ci à l'école
     for student in [paul, valerie, louis]:
         school.add_student(student)
 
     # création des cours
-    francais: Course = Course("Français", date(2024, 1, 29),
-                                          date(2024, 2, 16))
-    histoire: Course = Course("Histoire", date(2024, 2, 5),
-                                          date(2024, 2, 16))
-    geographie: Course = Course("Géographie", date(2024, 2, 5),
-                                              date(2024, 2, 16))
-    mathematiques: Course = Course("Mathématiques", date(2024, 2, 12),
-                                                    date(2024, 3, 8))
-    physique: Course = Course("Physique", date(2024, 2, 19),
-                                          date(2024, 3, 8))
-    chimie: Course = Course("Chimie", date(2024, 2, 26),
-                                      date(2024, 3, 15))
-    anglais: Course = Course("Anglais", date(2024, 2, 12),
-                                        date(2024, 2, 24))
-    sport: Course = Course("Sport", date(2024, 3, 4),
-                                    date(2024, 3, 15))
+    francais = Course(
+        "Français", date(2024, 1, 29), date(2024, 2, 16)
+    )
+    histoire = Course(
+        "Histoire", date(2024, 2, 5), date(2024, 2, 16)
+    )
+    geographie = Course(
+        "Géographie", date(2024, 2, 5), date(2024, 2, 16)
+    )
+    mathematiques = Course(
+        "Mathématiques", date(2024, 2, 12), date(2024, 3, 8)
+    )
+    physique = Course(
+        "Physique", date(2024, 2, 19), date(2024, 3, 8)
+    )
+    chimie = Course(
+        "Chimie", date(2024, 2, 26), date(2024, 3, 15)
+    )
+    anglais = Course(
+        "Anglais", date(2024, 2, 12), date(2024, 2, 24)
+    )
+    sport = Course(
+        "Sport", date(2024, 3, 4), date(2024, 3, 15)
+    )
 
     # ajout de ceux-ci à l'école
-    for course in [francais, histoire, geographie, mathematiques,
-                   physique, chimie, anglais, sport]:
+    for course in [
+        francais, histoire, geographie, mathematiques,
+        physique, chimie, anglais, sport
+    ]:
         school.add_course(course)
 
     # création des enseignants
-    victor  = Teacher('Victor', 'Hugo', 23, date(2023, 9, 4))
-    jules   = Teacher('Jules', 'Michelet', 32, date(2023, 9, 4))
-    sophie  = Teacher('Sophie', 'Germain', 25, date(2023, 9, 4))
-    marie   = Teacher('Marie', 'Curie', 31, date(2023, 9, 4))
+    victor = Teacher('Victor', 'Hugo', 23, date(2023, 9, 4))
+    jules = Teacher('Jules', 'Michelet', 32, date(2023, 9, 4))
+    sophie = Teacher('Sophie', 'Germain', 25, date(2023, 9, 4))
+    marie = Teacher('Marie', 'Curie', 31, date(2023, 9, 4))
     william = Teacher('William', 'Shakespeare', 34, date(2023, 9, 4))
-    michel  = Teacher('Michel', 'Platini', 42, date(2023, 9, 4))
+    michel = Teacher('Michel', 'Platini', 42, date(2023, 9, 4))
 
     # ajout de ceux-ci à l'école
     for teacher in [victor, jules, sophie, marie, william, michel]:
@@ -97,9 +107,9 @@ def main() -> None:
 Bienvenue dans notre école
 --------------------------""")
 
-    school: School = School()
+    school = School()
 
-    # initialisation d'un ensemble de cours, enseignants et élèves composant l'école
+    # initialisation d'un ensemble de cours, enseignants et élèves
     init_school(school)
 
     # affichage de la liste des cours, leur enseignant et leurs élèves
